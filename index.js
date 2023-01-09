dd2b17f4deba875e97e942fcb8c0c0de5efe1a26
Andy Tran <transient.tran@gmail.com>
2023-01-09 11:29:08 -0500

    attach Game to window to use in html

--- index.js
+++ index.js
@@ -1,37 +1,4 @@
-// import { CARDS } from "./cards.js";
-
-const CARDS = [
-  {
-    power: [3, 3, 4, 4],
-    rank: 1,
-    name: "Bomb",
-    sprite: "",
-  },
-  {
-    power: [1, 3, 4, 7],
-    rank: 1,
-    name: "Pixie",
-    sprite: "",
-  },
-  {
-    power: [6, 3, 8, 5],
-    rank: 2,
-    name: "Frog",
-    sprite: "",
-  },
-  {
-    power: [5, 5, 5, 5],
-    rank: 2,
-    name: "Fire Elemental",
-    sprite: "",
-  },
-  {
-    power: [8, 6, 7, 5],
-    rank: 3,
-    name: "Ifrit",
-    sprite: "",
-  },
-];
+import { CARDS } from "./cards.js";
 
 /**
  * Create a 3x3 board on the canvas
@@ -118,3 +85,5 @@ function calculate() {
     [6, 7, 8],
   ];
 }
+
+window.Game = Game;
